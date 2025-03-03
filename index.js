889c1f147b7ce8c576a3699684d8655ea6915279
Inshiya Ravat <inshiya.ravat@simformsolutions.com>
2025-03-03 16:21:37 +0530

    fix: empty todo addition, appearance of existing todo while editing

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ function $(el){
 
 function handleAddTodo(event){
     let todoInput = document.getElementById("todo").value
+    if(todoInput == ""){
+        alert('Add Todo in order to add it to the list!')
+        return false
+    }
     document.getElementById("todo").value = ""
     const ul = document.getElementById('todo-list')
     const li = document.createElement('li')
@@ -23,6 +27,7 @@ function handleAddTodo(event){
     editBtn.addEventListener("click",handelEdit)
     function handelEdit(){
         const editInput = document.createElement('input')
+        editInput.value = text.textContent
         text.replaceWith(editInput)
 
         const saveBtn = document.createElement('button')
@@ -32,6 +37,10 @@ function handleAddTodo(event){
         saveBtn.addEventListener("click", handleSave)
         function handleSave(){
             const updatedTodo = editInput.value
+            if(updatedTodo == ""){
+                alert('Add Todo in order to add it to the list!')
+                return false
+            }
             saveBtn.replaceWith(editBtn)
             editInput.replaceWith(updatedTodo)
         }
